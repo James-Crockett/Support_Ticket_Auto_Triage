925964b4a0cbb358d88ddb467cb1ce1b9da4057b
James-Crockett <nitheesh.gnk777@gmail.com>
2025-12-23 16:35:42 -0800

    docker_stuff

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, Form
 import joblib
 import os
+import sys
 
 app = FastAPI(title="Support Ticket Auto-Triage API")
-
-class Ticket(BaseModel):
-    subject: str
-    body: str
 
 model = None
 vectorizer = None
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MODEL_DIR = os.path.join(BASE_DIR, "models", "svm")
+MODEL_DIR = os.path.join(BASE_DIR, "models", "svm") 
+
+if not os.path.exists(MODEL_DIR):
+    MODEL_DIR = os.path.join(BASE_DIR, "models", "svm")
 
 try:
-    print(f"Loading models from: {MODEL_DIR}")
     vectorizer = joblib.load(os.path.join(MODEL_DIR, "vectorizer.pkl"))
     model = joblib.load(os.path.join(MODEL_DIR, "svm.pkl"))
-    print("Models loaded")
+    print("Models loaded successfully!")
 except Exception as e:
     print(f"Error loading models: {e}")
 
@@ -28,16 +26,19 @@ def health_check():
     return {"status": "ok", "model_loaded": model is not None}
 
 @app.post("/predict")
-def predict_ticket(ticket: Ticket):
+def predict_ticket(
+    subject: str = Form(..., description="Subject"),
+    body: str = Form(..., description="Content")
+):
     if not model or not vectorizer:
         raise HTTPException(status_code=500, detail="Model not loaded")
     
-    full_text = (ticket.subject or "") + "\n" + (ticket.body or "")
-    
+    full_text = f"{subject} \n {body}"
+
     vectorized_text = vectorizer.transform([full_text])
 
     prediction = model.predict(vectorized_text)[0]
     
     return {
-        "predicted_queue": prediction,
+        "predicted_queue": prediction
     }
